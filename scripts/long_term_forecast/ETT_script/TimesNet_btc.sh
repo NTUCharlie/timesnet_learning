7849593c1f392f2e0c92095457dac8eaba71c76b
NTUCharlie <changlei199295@gmai.com>
2025-03-29 14:58:56 +0800

    notebook checkpoint

--- scripts/long_term_forecast/ETT_script/TimesNet_btc.sh
+++ scripts/long_term_forecast/ETT_script/TimesNet_btc.sh
@@ -6,26 +6,26 @@ python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path btc.csv \
-  --model_id 'btc_96_96_nontimef_no_scale' \
+  --data_path btc_nopct.csv \
+  --model_id 'btc_96_96_target_close30_nontimef_withscale' \
   --model $model_name \
   --data 'custom' \
   --features 'MS' \
-  --target 'pct' \
+  --target 'close' \
   --freq 't' \
   --embed 'timeF' \
-  --batch_size 9600 \
-  --seq_len 96 \
+  --batch_size 3600 \
+  --seq_len 720 \
   --label_len 0 \
-  --pred_len 10 \
+  --pred_len 30 \
   --e_layers 2 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 16 \
+  --enc_in 15 \
   --dec_in 16 \
   --c_out 1 \
   --des 'Exp' \
-  --d_model 8 \
-  --d_ff 8 \
+  --d_model 16 \
+  --d_ff 4 \
   --top_k 4 \
   --itr 1
