fce9b166fecdafaf889db89ea08ae8be3e3cb7cb
NTUCharlie <changlei199295@gmai.com>
2025-03-30 22:20:02 +0800

    add constant learning rate

--- scripts/long_term_forecast/ETT_script/TimesNet_btc.sh
+++ scripts/long_term_forecast/ETT_script/TimesNet_btc.sh
@@ -1,31 +1,34 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 
-model_name=TimesNet
+model_name=TimesNet_norevin
 
 python -u run.py \
-  --task_name long_term_forecast \
+  --task_name long_term_forecast_no_revin \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path btc_nopct.csv \
-  --model_id 'btc_96_96_target_close30_nontimef_withscale' \
+  --data_path btc.csv \
+  --model_id 'btc_pct30_norevin' \
   --model $model_name \
   --data 'custom' \
   --features 'MS' \
-  --target 'close' \
+  --target 'pct' \
   --freq 't' \
   --embed 'timeF' \
-  --batch_size 3600 \
+  --batch_size 2048 \
   --seq_len 720 \
   --label_len 0 \
   --pred_len 30 \
-  --e_layers 2 \
+  --e_layers 1 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in 15 \
+  --enc_in 16 \
   --dec_in 16 \
   --c_out 1 \
   --des 'Exp' \
   --d_model 16 \
-  --d_ff 4 \
+  --d_ff 8 \
   --top_k 4 \
-  --itr 1
+  --num_kernels 6 \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --lradj constant
--- utils/tools.py
+++ utils/tools.py
@@ -9,7 +9,7 @@ import math
 plt.switch_backend('agg')
 
 
-def adjust_learning_rate(optimizer, epoch, args):
+def  adjust_learning_rate(optimizer, epoch, args):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
     if args.lradj == 'type1':
         lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
@@ -22,6 +22,8 @@ def adjust_learning_rate(optimizer, epoch, args):
         lr_adjust = {epoch: args.learning_rate if epoch < 3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
     elif args.lradj == "cosine":
         lr_adjust = {epoch: args.learning_rate /2 * (1 + math.cos(epoch / args.train_epochs * math.pi))}
+    elif args.lradj == "constant":
+        lr_adjust = {epoch: args.learning_rate}
     if epoch in lr_adjust.keys():
         lr = lr_adjust[epoch]
         for param_group in optimizer.param_groups:
